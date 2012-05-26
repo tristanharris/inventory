01871c2398053f94822d698e270f972e148e1b55
Tristan Harris <tris.dev@pumpcottage.me.uk>
2012-05-26 17:08:05 +0100

    Initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inventory::Application.config.secret_token = '8b0f0ae9455bbe132e5a93117f6ae79a068f298fb8ea3cd94161d8384707db781a3c6e82ab9e18b7babcbd25d1b5ea4e490807302f2b54dadbd72ff27c0117ec'
