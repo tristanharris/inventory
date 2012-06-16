a07305236e41effdf0d6719ed96df9f3d0be5e59
Tristan Harris <tris.dev@pumpcottage.me.uk>
2012-06-16 17:21:57 +0100

    Fix bug in image removing

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
-  attr_accessible :description, :location, :name, :store_image, :usage_image
+  attr_accessible :description, :location, :name, :store_image, :usage_image, :remove_store_image, :remove_usage_image
 
   mount_uploader :store_image, ItemImageUploader
   mount_uploader :usage_image, ItemImageUploader
