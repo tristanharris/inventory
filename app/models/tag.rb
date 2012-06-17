3d3fb0157908dbde838be5f015d94d70156c9e26
Tristan Harris <tris.dev@pumpcottage.me.uk>
2012-06-17 21:25:58 +0100

    Add items to tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :item_ids
 
   has_many :items_tags
   has_many :items, :through => :items_tags
