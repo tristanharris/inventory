0c7a0eff5201b343ac0e31131e1e39bac0dbf175
Tristan Harris <tris.dev@pumpcottage.me.uk>
2012-06-18 22:56:18 +0100

    Improve selecting location

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -1,15 +1,30 @@
 $('#location_edit [owner=district] [location]').live('click', function(e) {
   e.preventDefault()
-  $('#location').attr('value', $(this).attr('location'));
-
-  var selected = $('#shelf').val();
-  $('#shelf').empty();
-  for (i=1; i<=$(this).attr('shelves'); i++) {
-    $('#shelf').prepend($('<option></option>').attr("value", i).text(i));
-  }
-  $('#shelf').val(selected);
+  select_location($(this));
 });
 
 $('#location_edit .edit_item input[type=submit]').live('click', function(e) {
   $('#item_location').attr('value', $('#location').attr('value') + ':' + $('#shelf').val());
 });
+
+$(function() {
+  var parts = $('#item_location').attr('value').split(':');
+  select_location($('[location='+parts[0]+']'));
+  $('#shelf').attr('value', parts[1]);
+});
+
+function update_shelf(loc) {
+  var selected = $('#shelf').val();
+  $('#shelf').empty();
+  for (i=1; i<=loc.attr('shelves'); i++) {
+    $('#shelf').prepend($('<option></option>').attr("value", i).text(i));
+  }
+  $('#shelf').val(selected);
+}
+
+function select_location(loc) {
+  $('#location').attr('value', loc.attr('location'));
+  $('svg [selected]').removeAttr('selected');
+  loc.attr('selected', 'true');
+  update_shelf(loc);
+}
