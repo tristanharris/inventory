32528f84eb29768d21e2ad1872a51769d04c5b8c
Tristan Harris <tris.dev@pumpcottage.me.uk>
2012-08-05 14:49:56 +0100

    Add image grid view for items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     render :partial => 'items/image_link', :locals => {:item => item, :type => type} if @item.send(type.to_s + '_image?')
   end
 
+  def image_for(item, options={})
+    types = [:usage, :store]
+    types.keep_if{|t| item.send(t.to_s + '_image?')}
+    image_tag item.send(types.first.to_s + '_image').thumb.url, options unless types.empty?
+  end
+
+  def image_index?
+    true
+  end
+
 end
