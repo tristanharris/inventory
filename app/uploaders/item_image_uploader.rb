9ff879dcda7af18ef9473c9e67c2f5abdb679194
Tristan Harris <tris.dev@pumpcottage.me.uk>
2012-08-05 16:11:22 +0100

    Rotate images correctly

--- app/uploaders/item_image_uploader.rb
+++ app/uploaders/item_image_uploader.rb
@@ -10,10 +10,18 @@ class ItemImageUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
+  process :auto_orient
   process :resize_to_limit => [800, 800]
 
   version :thumb do
     process :resize_to_fit => [100,100]
+  end
+
+  def auto_orient
+    manipulate! do |img|
+      img.auto_orient
+      img
+    end
   end
 
 end
