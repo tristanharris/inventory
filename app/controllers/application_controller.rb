bc3d290a66e4cd7b41418f2d49d6a75ec2cf1021
Tristan Harris <tris.dev@pumpcottage.me.uk>
2013-03-01 20:57:06 +0000

    Improve authorization failure response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   http_basic_authenticate_with :name => ENV['auth_user'], :password => ENV['auth_pass']
   check_authorization :unless => :devise_controller?
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to new_user_session_url, :alert => exception.message
+    if current_user
+      redirect_to :back, :alert => exception.message
+    else
+      redirect_to new_user_session_url, :alert => exception.message
+    end
   end
 
   before_filter :check_style
