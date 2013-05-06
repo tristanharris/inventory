f949a5db3fe6ce0ab9d7735c09b9eff074f77d35
Tristan Harris <tris.dev@pumpcottage.me.uk>
2013-05-06 10:51:59 +0100

    Remove secret token from repo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Inventory::Application.config.secret_token = '8b0f0ae9455bbe132e5a93117f6ae79a068f298fb8ea3cd94161d8384707db781a3c6e82ab9e18b7babcbd25d1b5ea4e490807302f2b54dadbd72ff27c0117ec'
+raise 'No ENV["secret_token"] set' if ENV['secret_token'].blank?
+Inventory::Application.config.secret_token = ENV['secret_token']
