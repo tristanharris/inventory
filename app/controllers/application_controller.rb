5f7d978af18e5776d105c2f81f59a2621a2e5691
Tristan Harris <tris.dev@pumpcottage.me.uk>
2013-05-06 11:14:59 +0100

    Remove basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  http_basic_authenticate_with :name => ENV['auth_user'], :password => ENV['auth_pass']
 
   before_filter :require_login
 
